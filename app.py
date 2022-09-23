b1ff1c6e4ef84490d631fbd4b6c5638f8c002cb4
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2022-09-23 17:12:37 -0600

    updated

--- app.py
+++ app.py
@@ -21,23 +21,17 @@ def enter_selection():
     print('Please enter your selection:')
     user_selection = input()
     try:
-        if(user_selection > 4 or user_selection < 1):
-            user_selection = int(user_selection)
-    except:
-        print('Please enter a valid selection from 1 to 4')
-        print('Please enter your first number:')
-        num1 = input()
-        num1 = int(num1)
-        print('Please enter your second number: ')
-        num2 = input()
-        num2 = int(num2)
-    return [user_selection , num1, num2]
+        user_selection = int(user_selection)
+        if(user_selection >= 1 and user_selection <= 4 ):
+            return user_selection
+        else:
+            print('Not a valid selection')
+    except ValueError:
+        print('Please select valid option')
+    return user_selection
 
-values = enter_selection()
+calculation_type = enter_selection()
 
-calculation_type = values[0]
-first_number = values[1]
-second_number = values[2]
 
 if(calculation_type == 1):
     print('Your result: ',add_numbers(first_number,second_number))
