7fef2fb6a2b9137d6745713746f5a07728d33a9b
satinder <satindersingh472@gmail.com>
2022-09-24 08:06:19 -0600

    finished

--- app.py
+++ app.py
@@ -10,56 +10,49 @@ def multiply_numbers(num1,num2):
 def divide_numbers(num1,num2):
     return num1 / num2     
 
+def get_inputs():
+    inputs_array= []
+    print('Please add the first number: ')
+    x = input()
+    x = int(x)
+    inputs_array.append(x)
+    print('Please add the second number: ')
+    y = input()
+    y = int(y)
+    inputs_array.append(y)
+    return inputs_array
 
-print('Welcome to the simple calculator, Please select from the following options:')
-print('1: Addition')
-print('2: Substraction')
-print('3: Multiplication')
-print('4: Division')
+def do_maths(calculation_type,x,y):
+    if(calculation_type == 1):
+        print('Your result: ',add_numbers(x,y))
+    elif(calculation_type == 2):
+        print('Your result: ',subtract_numbers(x,y))
+    elif(calculation_type == 3):
+        print('Your result: ',multiply_numbers(x,y))
+    elif(calculation_type == 4):
+        print('Your result: ',divide_numbers(x,y))
+
 
 def enter_selection():
+    print('Welcome to the simple calculator, Please select from the following options:')
+    print('1: Addition')
+    print('2: Substraction')
+    print('3: Multiplication')
+    print('4: Division')
     print('Please enter your selection:')
     user_selection = input()
     try:
         user_selection = int(user_selection)
         if(user_selection >= 1 and user_selection <= 4 ):
-            return user_selection
-    except TypeError:
-        print('Not a valid option')
-        user_selection = 0
-        return user_selection
+            inputs =  get_inputs()
+            do_maths(user_selection,inputs[0],inputs[1])
+        else:
+            print('Number did not match:  Not a valid calculation type, selection should be from 1 to 4')
+            enter_selection()
+    except:
+        print('ValueError: Not able to calculate alphabets, selection should contain numbers only')
+        enter_selection()
 
-calculation_type = enter_selection()
+dbms = enter_selection()
 
-def get_inputs():
-    inputs_array= []
-    print('Please add the first number: ')
-    num1 = input()
-    num1 = int(num1)
-    inputs_array.append(num1)
-    print('Please add the second number: ')
-    num2 = input()
-    num2 = int(num2)
-    inputs_array.append(num2)
-    return inputs_array
 
-inputs = []
-first_number = 0
-second_number = 0
-try:
-    if(calculation_type >= 1 and calculation_type <= 4):
-        inputs = get_inputs()
-except: 
-    print('Not the valid selection of calculation type')
-    inputs = 0
-    first_number = inputs[0]
-    second_number = inputs[1]
-
-if(calculation_type == 1):
-    print('Your result: ',add_numbers(first_number,second_number))
-elif(calculation_type == 2):
-    print('Your result: ',subtract_numbers(first_number,second_number))
-elif(calculation_type == 3):
-    print('Your result: ',multiply_numbers(first_number,second_number))
-elif(calculation_type == 4):
-    print('Your result: ',divide_numbers(first_number,second_number))
